e765b0ce28b372e5ec9070ad2e896e6b2fda393f
Nicolás Espinosa <ne_espinosa@hotmail.com>
2021-08-30 23:03:46 -0300

    Se agrega button de Crear y modificacion de estilo

--- View/rpcview/src/components/Medicamento.js
+++ View/rpcview/src/components/Medicamento.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Table, Button } from 'react-bootstrap';
+import { Table, Button, Container, Row, Col } from 'react-bootstrap';
 
 function Medicamento() {
 
@@ -31,34 +31,44 @@ function Medicamento() {
 
     console.log(medicamentos);
     return (
-        <div className="d-flex p-5 justify-content-center">
-            <Table striped bordered hover variant="dark" size="sm" responsive>
-                <thead>
-                    <tr>
-                        <th>#Codigo</th>
-                        <th>Nombre Comercial</th>
-                        <th>Nombre Genérico</th>
-                        <th>Tipo Medicamento</th>
-                        <th>Acción</th>
-                    </tr>
-                </thead>
-                {
-                    medicamentos.map(medicamento => {
-                        return (
-                            <tbody key={medicamento.id}>
-                                <tr>
-                                    <td>{medicamento.codigo}</td>
-                                    <td>{medicamento.nomComercial}</td>
-                                    <td>{medicamento.nomDroga}</td>
-                                    <td>{medicamento.tipoMedicamento[0].nombre}</td>
-                                    <td><Button variant="danger">Eliminar</Button></td>
-                                </tr>
-                            </tbody>
-                        )
-                    })
-                }
-            </Table>
-        </div>
+        <Container>
+            <Col>
+                <div className="d-flex p-3 justify-content-end">
+                    <Button className="float-right" variant="warning">Crear</Button>
+                </div>
+            </Col>
+            <Col>
+                <div className="d-flex p-3 justify-content-center">
+
+                    <Table responsive="sm" striped bordered hover variant="dark" size="sm">
+                        <thead>
+                            <tr>
+                                <th>#Codigo</th>
+                                <th>Nombre Comercial</th>
+                                <th>Nombre Genérico</th>
+                                <th>Tipo Medicamento</th>
+                                <th>Acción</th>
+                            </tr>
+                        </thead>
+                        {
+                            medicamentos.map(medicamento => {
+                                return (
+                                    <tbody key={medicamento.id}>
+                                        <tr>
+                                            <td>{medicamento.codigo}</td>
+                                            <td>{medicamento.nomComercial}</td>
+                                            <td>{medicamento.nomDroga}</td>
+                                            <td>{medicamento.tipoMedicamento[0].nombre}</td>
+                                            <td><Button variant="danger">Eliminar</Button></td>
+                                        </tr>
+                                    </tbody>
+                                )
+                            })
+                        }
+                    </Table>
+                </div>
+            </Col>
+        </Container>
     )
 }
 
